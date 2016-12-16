158590f46521411946703c9c89ebb33f8a372b7c
Sergey Shpak <serge1272@mail.ru>
2016-12-16 16:46:25 +0100

    Add comments

--- include/lexical_analysis/utils.h
+++ include/lexical_analysis/utils.h
@@ -5,8 +5,40 @@
 
 #include "scanner_utils.h"
 
+/**
+ * \brief Outputs an error message to stderr.
+ *
+ * Outputs an error message to stderr. The resulting string's format is 
+ * defined inside the function.
+ *
+ * \param[in] msg The memory to be output.
+ * \retval 0  String was ouput succesfully.
+ * \retval -1 String could not be output.
+ */
 int lex_error(char *msg);
-int lex_token_error(char *msg_template, int line_num, int char_pos);
+
+/**
+ * \brief Outpus an error message prodcued because of a bad token found in the
+ * scanned file.
+ *
+ * Outputs an error message about a bad token that occurs in the scanned file.
+ * The message contains the token starting position. The message is written to
+ * stderr.
+ *
+ * \param[in] explanation String that is appended to the error message.
+ * \param[in] line_num  Number of line in the file on which the bad token 
+ *            was found
+ * \param[in] char_pos  Position of the bad token in the line on which it was
+ *            found.
+ *
+ * \returns 0 if the error has been succesfully output. Other value in case of
+ *            an error.
+ */
+int lex_token_error(char *explanation, int line_num, int char_pos);
+
+/**
+ * \brief Prints a given 
+ */
 int lex_log(char *msg, FILE*);
 
 /**
